b7f3a3817e458b407a0297591c9ba526c297c354
riricardo <ricardo.augusto.cardona@gmail.com>
2025-11-30 16:33:40 +0000

    Corrections in the path duo to app name chage.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,6 +35,9 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        ndk {
+            abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86_64")
+        }
     }
 
     signingConfigs {
@@ -52,6 +55,11 @@ android {
 
             isMinifyEnabled = true
             isShrinkResources = true
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                file("proguard-rules.pro")
+            )
         }
 
         getByName("debug") {
--- android/app/src/main/kotlin/com/cardonatech/ctrainer/MainActivity.kt
+++ android/app/src/main/kotlin/com/cardonatech/ctrainer/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.ctrainer
+package com.cardonatech.ctrainer
 
 import io.flutter.embedding.android.FlutterActivity
 
